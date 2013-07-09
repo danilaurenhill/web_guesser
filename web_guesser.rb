4b1a6742d64fda110078a70c091baea4e2e82b32
Danielle Hill <starson3@aol.com>
2013-07-09 16:50:02 -0400

    End of the day Tuesday.

--- web_guesser.rb
+++ web_guesser.rb
@@ -4,6 +4,12 @@ require 'sinatra/reloader'
 number = (1 + rand(99)).to_s
 
 get "/" do 
+   params["guess.to_s"]
    erb :index, :locals => {:number => number}
+end
+
+if number.to_i > 99
+	puts "Your guess is too high!!!!!"
+    erb :index, :locals => {:number => number}
 end
 
